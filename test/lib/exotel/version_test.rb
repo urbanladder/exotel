11a5cf2fca1b1a73edd9d5d840fe8921d765638c
anshul <anshul@urbanladder.com>
2013-08-26 18:47:19 +0530

    Changed indentation to two spaces throughout

--- test/lib/exotel/version_test.rb
+++ test/lib/exotel/version_test.rb
@@ -1,7 +1,7 @@
 require_relative '../../test_helper'
  
 describe Exotel do
-    it "must be defined" do
-        Exotel::VERSION.wont_be_nil
-    end
+  it "must be defined" do
+    Exotel::VERSION.wont_be_nil
+  end
 end
